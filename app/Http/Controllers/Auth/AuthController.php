0c86b8cb17df9a44c642f8b649d76e26ad7d1956
Learning Laravel <support@learninglaravel.net>
2017-11-17 23:36:01 +0700

    Recipe 4

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+namespace App\Http\Controllers\Auth;
+
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+use Socialite;
+use Auth;
+
+class AuthController extends Controller
+{
+    protected $redirectTo = '/';
+
+    public function __construct()
+    {
+        $this->middleware('guest', ['except' => 'logout']);
+    }
+
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|confirmed|min:6',
+        ]);
+    }
+
+    public function redirectToFacebook()
+    {
+        return Socialite::with('facebook')->redirect();
+    }
+
+    public function getFacebookCallback()
+    {
+        $data = Socialite::with('facebook')->user();
+        $user = User::where('email', $data->email)->first();
+        if (!is_null($user)) {
+            Auth::login($user);
+            $user->name = $data->user['name'];
+            $user->provider_id = $data->user['id'];
+            $user->save();
+        } else {
+            $user = User::where('provider_id', $data->user['id'])->first();
+            if (is_null($user)) {
+                // Create a new user
+                $user = new User();
+                $user->name = $data->user['name'];
+                $user->email = $data->email;
+                $user->provider_id = $data->user['id'];
+                $user->save();
+            }
+            Auth::login($user);
+        }
+        return redirect('/')->with('success', 'Successfully logged in!');
+    }
+}
