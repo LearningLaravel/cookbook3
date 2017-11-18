ac460bb9553ba48e011c02432cc11508e9d1248d
Learning Laravel <support@learninglaravel.net>
2017-11-18 12:36:08 +0700

    Recipe 9

---
+++ tests/Feature/BlogTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class BlogTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBlogResponseIsValid()
+    {
+        $response = $this->get('/json');
+        $response->assertStatus(200);
+    }
+}
